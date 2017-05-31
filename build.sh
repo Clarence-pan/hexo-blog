6743dbb210409dd4626812e52d6c8cd338b6b90e
clarence <clarence.pchy@gmail.com>
2017-05-31 17:25:57 +0800

    auto commit

--- build.sh
+++ build.sh
@@ -12,7 +12,9 @@ if [ "$?" != "0" ]; then
 	exit 1;
 fi
 
-./node_modules/.bin/hexo generate
-cp -f source/test/* public/test/
-cp -f source/lib/* public/lib/
-cp -f source/fe-lab/* public/fe-lab/
+node build.js
+
+#./node_modules/.bin/hexo generate
+#cp -f source/test/* public/test/
+#cp -f source/lib/* public/lib/
+#cp -f source/fe-lab/* public/fe-lab/
